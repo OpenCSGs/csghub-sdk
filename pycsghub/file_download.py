21646f951ebf8c6c7bc75a7f896e6931ace79e4d
lumiere <lumiere@limgrave.local>
2024-06-03 23:08:23 +0800

    add  single file download API

--- pycsghub/file_download.py
+++ pycsghub/file_download.py
@@ -1,14 +1,23 @@
 import tempfile
 from functools import partial
 from http.cookiejar import CookieJar
+from pathlib import Path
+from typing import Optional, Union, List, Dict
 
 import requests
+from huggingface_hub.utils import filter_repo_objects
 from requests.adapters import Retry
 from tqdm import tqdm
-from pycsghub.utils import build_csg_headers
-from pycsghub.constants import API_FILE_DOWNLOAD_RETRY_TIMES, API_FILE_DOWNLOAD_TIMEOUT, API_FILE_DOWNLOAD_CHUNK_SIZE
+
+from pycsghub import utils
+from pycsghub.cache import ModelFileSystemCache
+from pycsghub.utils import build_csg_headers, get_cache_dir, model_id_to_group_owner_name, pack_model_file_info, \
+    get_file_download_url
+from pycsghub.constants import API_FILE_DOWNLOAD_RETRY_TIMES, API_FILE_DOWNLOAD_TIMEOUT, API_FILE_DOWNLOAD_CHUNK_SIZE, \
+    DEFAULT_REVISION
 from pycsghub.errors import FileDownloadError
 import os
+from pycsghub.errors import InvalidParameter
 
 
 def try_to_load_from_cache():
@@ -26,6 +35,91 @@ def csg_hub_download():
 def get_csg_hub_url():
     pass
 
+def file_download(
+        repo_id: str,
+        *,
+        file_name: str = None,
+        revision: Optional[str] = DEFAULT_REVISION,
+        cache_dir: Union[str, Path, None] = None,
+        local_files_only: Optional[bool] = False,
+        cookies: Optional[CookieJar] = None,
+        allow_patterns: Optional[Union[List[str], str]] = None,
+        ignore_patterns: Optional[Union[List[str], str]] = None,
+        headers: Optional[Dict[str, str]] = None,
+        endpoint: Optional[str] = None,
+        token: Optional[str] = None
+) -> str:
+    if cache_dir is None:
+        cache_dir = get_cache_dir()
+    if isinstance(cache_dir, Path):
+        cache_dir = str(cache_dir)
+    temporary_cache_dir = os.path.join(cache_dir, 'temp')
+    os.makedirs(temporary_cache_dir, exist_ok=True)
+
+    if file_name is None:
+        raise InvalidParameter('file_name cannot be None, if you want '
+                               'to load single file from repo {}'.format(repo_id))
+
+    group_or_owner, name = model_id_to_group_owner_name(repo_id)
+    name = name.replace('.', '___')
+
+    cache = ModelFileSystemCache(cache_dir, group_or_owner, name)
+
+    if local_files_only:
+        if len(cache.cached_files) == 0:
+            raise ValueError(
+                'Cannot find the requested files in the cached path and outgoing'
+                ' traffic has been disabled. To enable model look-ups and downloads'
+                " online, set 'local_files_only' to False.")
+        return cache.get_root_location()
+    else:
+        # make headers
+        # todo need to add cookies？
+        repo_info = utils.get_repo_info(repo_id,
+                                        revision=revision,
+                                        token=token,
+                                        endpoint=endpoint if endpoint is not None else get_endpoint())
+
+        assert repo_info.sha is not None, "Repo info returned from server must have a revision sha."
+        assert repo_info.siblings is not None, "Repo info returned from server must have a siblings list."
+        model_files = list(
+            filter_repo_objects(
+                items=[f.rfilename for f in repo_info.siblings],
+                allow_patterns=allow_patterns,
+                ignore_patterns=ignore_patterns,
+            )
+        )
+        if file_name not in model_files:
+            raise InvalidParameter('file {} not in repo {}'.format(file_name, repo_id))
+
+        with tempfile.TemporaryDirectory(
+                dir=temporary_cache_dir) as temp_cache_dir:
+            model_file_info = pack_model_file_info(file_name, revision)
+            if not cache.exists(model_file_info):
+                file_name = os.path.basename(model_file_info['Path'])
+                # get download url
+                url = get_file_download_url(
+                    model_id=repo_id,
+                    file_path=file_name,
+                    revision=revision)
+                # todo support parallel download api
+                http_get(
+                    url=url,
+                    local_dir=temp_cache_dir,
+                    file_name=file_name,
+                    headers=headers,
+                    cookies=cookies,
+                    token=token)
+
+                # todo using hash to check file integrity
+                temp_file = os.path.join(temp_cache_dir, file_name)
+                cache.put_file(model_file_info, temp_file)
+            else:
+                print(
+                    f'File {file_name} already in cache, skip downloading!'
+                )
+        cache.save_model_version(revision_info={'Revision': revision})
+        return os.path.join(cache.get_root_location())
 
 def http_get(*,
              url: str,
