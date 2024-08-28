f9c366f3fa411e9d283d7d73921c3cae7c9dcd3d
Sean <38311363@qq.com>
2024-08-28 14:00:19 +0800

    set version 0.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='csghub-sdk',
-    version='0.3.2',
+    version='0.3.3',
     author="opencsg",
     author_email="contact@opencsg.com",
     long_description=long_description,
