8031809b65bd3f331908ba589967d21383e56cc4
Sean <38311363@qq.com>
2025-03-30 11:37:53 +0800

    feat(finetune): add BERT fine-tuning script
    - Implement BERT fine-tuning process
    - Add data preprocessing function
    - Configure training arguments
    - Include model upload functionality
    - Create script for other models usage

---
+++ run_others_models.py
@@ -0,0 +1,21 @@
+
+# import os
+# os.environ['HF_ENDPOINT'] = 'https://hub.opencsg.com/hf'
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from pycsghub.repo_reader import AutoModelForCausalLM, AutoTokenizer
+
+mid = 'wanghh2000/MyMind-0.05B'
+tokenizer = AutoTokenizer.from_pretrained(mid)
+model = AutoModelForCausalLM.from_pretrained(mid, trust_remote_code=True)
+
+messages = []
+messages.append({"role": "user", "content": "你擅长哪一个学科？"})
+
+new_prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)[-8191:]
+
+inputs = tokenizer.encode(new_prompt, return_tensors="pt")
+outputs = model.generate(inputs)
+result = tokenizer.batch_decode(outputs)
+# print('<<< result >>>')
+print(result[0])
