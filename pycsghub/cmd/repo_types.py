db41d94cf5ce33d4519d810b927cbab133b769a2
SeanHH86 <154984842+SeanHH86@users.noreply.github.com>
2025-04-02 11:02:51 +0800

    Support download space (#73)
    * Support download space
    
    Added REPO_TYPE_SPACE to REPO_TYPES list.
    Updated dataset_info to use opencsg.com.
    Updated space_info to use opencsg.com.
    Updated model_info to use opencsg.com.
    Updated get_file_download_url for spaces.
    
    * add SPACE to RepoType enum
    
    - Added SPACE to RepoType enum
    - Imported REPO_TYPE_SPACE constant

--- pycsghub/cmd/repo_types.py
+++ pycsghub/cmd/repo_types.py
@@ -1,7 +1,8 @@
 import typer
 from enum import Enum
-from pycsghub.constants import REPO_TYPE_MODEL, REPO_TYPE_DATASET
+from pycsghub.constants import REPO_TYPE_MODEL, REPO_TYPE_DATASET, REPO_TYPE_SPACE
 
 class RepoType(str, Enum):
     MODEL = REPO_TYPE_MODEL
     DATASET = REPO_TYPE_DATASET
+    SPACE = REPO_TYPE_SPACE
