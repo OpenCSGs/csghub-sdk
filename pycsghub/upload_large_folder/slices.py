9160168d38c7bf3f01d91119f9fa6096504b38d8
Sean <38311363@qq.com>
2025-06-05 17:26:33 +0800

    fix(upload): handle 404 status in uploads
    - Log error for non-200 and non-404 responses
    - Raise error for non-404 responses only
    - Update error message for upload verification

--- pycsghub/upload_large_folder/slices.py
+++ pycsghub/upload_large_folder/slices.py
@@ -58,9 +58,11 @@ def slices_upload_complete(item: JOB_ITEM_T, uploaded_ids_map: Dict):
         ]
     }
     response = requests.post(metadata.lfs_upload_complete_url, json=payload)
-    if response.status_code != 200:
-        logger.error(f"LFS {paths.file_path} slices merge complete on {metadata.lfs_upload_complete_url} response: {response.text}")
-    response.raise_for_status()
+    if response.status_code != 200 and response.status_code != 404:
+        logger.error(f"LFS {paths.file_path} merge all slices complete on {metadata.lfs_upload_complete_url} response: {response.text}")
+    if response.status_code != 404:
+        response.raise_for_status()
+    return response.json()
 
 def slices_upload_verify(item: JOB_ITEM_T):
     paths, metadata = item
@@ -72,5 +74,6 @@ def slices_upload_verify(item: JOB_ITEM_T):
     verify_header = metadata.lfs_upload_verify.get("header")
     response = requests.post(verify_url, headers=verify_header, json=payload)
     if response.status_code != 200:
-        logger.error(f"LFS {paths.file_path} slices upload verify on {verify_url} response: {response.text}")
+        logger.error(f"LFS {paths.file_path} slices upload verify on {verify_url} response: {response.text}, delete file {paths.metadata_path} and retry")
     response.raise_for_status()
+    return response.json()
