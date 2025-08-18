f3fda2d1ba69f3f0ba75aa42bc0192841da71143
Sean <38311363@qq.com>
2025-08-18 20:36:06 +0800

    fix(download_dataset): remove allow_patterns argument
    - Removed allow_patterns from snapshot_download call

--- examples/download_dataset.py
+++ examples/download_dataset.py
@@ -7,6 +7,4 @@ endpoint = "https://hub.opencsg.com"
 repo_id = "OpenDataLab/CodeExp"
 repo_type = "dataset"
 cache_dir = "/Users/hhwang/temp/"
-allow_patterns = ["*.json"]
-result = snapshot_download(repo_id, repo_type=repo_type, cache_dir=cache_dir, endpoint=endpoint, token=token,
-                           allow_patterns=allow_patterns)
+result = snapshot_download(repo_id, repo_type=repo_type, cache_dir=cache_dir, endpoint=endpoint, token=token)
