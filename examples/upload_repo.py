b776e4c1306505c60f33af96e4810691608abbbf
HaiHui <154984842+HaiHui886@users.noreply.github.com>
2025-11-28 15:56:48 +0800

    Add repository parameter to support for space uploads (#110)
    - Added parameters for sdk, cluster_id
    - Included resource_id and min_replica
    - Updated example for space upload
    - Changed dataset repo_id in example
    - Bumped version to 0.7.8
    
    Co-authored-by: Haihui.Wang <wanghh2000@163.com>

--- examples/upload_repo.py
+++ examples/upload_repo.py
@@ -1,8 +1,7 @@
 import logging
 from pycsghub.repository import Repository
 
-# token = "your access token"
-token = None
+token = "your access token"
 
 # set log level
 logging.basicConfig(
@@ -12,12 +11,26 @@ logging.basicConfig(
     handlers=[logging.StreamHandler()]
 )
 
+# for dataset
 r = Repository(
-    repo_id="wanghh2000/ds16",
-    upload_path="/Users/hhwang/temp/bbb/jsonl",
+    repo_id="wanghh2000/dataset16",
+    upload_path="/Users/hhwang/temp/bbb",
     user_name="wanghh2000",
     token=token,
     repo_type="dataset",
+)
+
+# for space
+r = Repository(
+    repo_id="wanghh2000/space16",
+    upload_path="/Users/hhwang/temp/bbb",
+    user_name="wanghh2000",
+    token=token,
+    repo_type="space",
+    sdk="gradio",
+    resource_id=4,
+    cluster_id="xxxx-xxxx-xxxx-xxxx",
+    min_replica=1,
 )
 
 r.upload()
